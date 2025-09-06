7ef15d8e00be8739f49974f5f3a5ea007e4eede0
Neelam <kausarneelam@gmail.com>
2025-09-06 12:27:49 +0500

    Remove root-level backend files

--- routes/SignUp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/api/SignUp', (req, res) => {
-  // your logic here
-  res.json({ message: "Signup successful!" });
-});
-
-module.exports = router;
